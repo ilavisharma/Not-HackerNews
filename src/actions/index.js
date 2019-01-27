6de692a8206e2ec0a1bb28710a0d67dc8fcd0b8e
Lavi <lavisharma796@gmail.com>
2019-01-27 20:08:02 +0000

    add new actions for askHN

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,8 @@
-// import _ from 'lodash';
 import hackerNews from '../apis/hackerNews';
 
 export const fetchNewsId = () => async dispatch => {
     const response = await hackerNews.get('/topstories.json?print=pretty');
-    response.data.splice(20);
+    response.data.splice(40);
     dispatch({
         type: 'FETCH_NEWS_ID',
         payload: response.data
@@ -29,4 +28,32 @@ export const fetchNewsAndId = () => async(dispatch, getState) => {
     await dispatch(fetchNewsId());
 
     getState().newsId.forEach(id => dispatch(fetchNewsFromId(id)));
+};
+
+export const fetchAskId = () => async dispatch => {
+    const response = await hackerNews.get('/askstories.json?print=pretty');
+    dispatch({
+        type: 'FETCH_ASK_ID',
+        payload: response.data
+    });
+};
+
+export const fetchAskFromId = (askId) => async dispatch => {
+    const response = await hackerNews.get(`/item/${askId}.json?print=pretty`);
+    dispatch({
+        type: 'FETCH_ASK',
+        payload: {
+            id: response.data.id,
+            title: response.data.title,
+            by: response.data.by,
+            score: response.data.score,
+            text: response.data.text
+        }
+    });
+};
+
+export const fetchAskAndId = () => async(dispatch, getState) => {
+    await dispatch(fetchAskId());
+
+    getState().askId.forEach(id => dispatch(fetchAskFromId(id)));
 };
